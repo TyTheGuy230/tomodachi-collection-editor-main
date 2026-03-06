dd78204aa2af578cb13fb3b72888ad25e232f850
TyTheGuy230 <141195336+TyTheGuy230@users.noreply.github.com>
2026-03-06 17:58:28 -0500

    added tdc style buttons

--- code/button.gd
+++ code/button.gd
@@ -1,5 +1,7 @@
-extends Button
+extends TextureButton
 @onready var parts = $"../../../body2"
+@onready var button1 = $"."
+@onready var button2 = $head2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,23 +12,22 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	if parts.head_id == 1:
+		button1.button_pressed = true
+	else:
+		button1.button_pressed = false
+		
+	if parts.head_id == 2:
+		button2.button_pressed = true
+	else:
+		button2.button_pressed = false
 
 
 func _on_pressed() -> void:
-	parts.set_head(1)
+	parts.head_id = 1
 	parts.load_head()
 
-
-func _on_button_2_pressed() -> void:
-	parts.set_head(2)
+func _on_head_2_pressed() -> void:
+	parts.head_id = 2
 	parts.load_head()
-
-
-func _on_button_3_pressed() -> void:
-	parts.set_hair(1)
-	parts.load_hair()
-
-
-func _on_button_4_pressed() -> void:
-	parts.set_hair(71)
-	parts.load_hair()
